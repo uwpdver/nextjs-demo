1c0b8dd637665303b22dc9bc6aac246e2c0ec452
zhihao <1119548217@qq.com>
2022-03-27 21:53:16 +0800

    feat: 增加步骤条组件

---
+++ components/steps/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const Steps = ({ curIndex = 0, steps = [] }) => {
+  const stepRender = ({ title }, index) => {
+    return (
+      <div className='overflow-x-hidden step-item flex-1 pl-4 first:pl-0 last:flex-none group'>
+        <div>
+          <div className={classNames('step-item-title relative inline-block pr-4 group-last:pr-0 after:bg-gray-800 after:absolute after:left-full after:top-1/2', {
+            'after:opacity-10': index >= curIndex
+          })} >{title}</div>
+          <style jsx>
+            {`
+              .step-item-title::after {
+                content: "";
+                width: 9999px;
+                height: 1px;
+              }
+            `}
+          </style>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className='flex steps-container'>
+      {steps.map(stepRender)}
+    </div>
+  )
+}
+
+export default Steps;
--- pages/buy/index.js
+++ pages/buy/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import Layout from "../../components/layout";
+import Steps from '../../components/steps';
 
-const Buy = ({}) => {
+const steps = [
+  {
+    title: '填写收货地址',
+  },
+  {
+    title: '提交订单',
+  },
+  {
+    title: '选择付款方式',
+  },
+  {
+    title: '完成购买',
+  },
+]
+
+const Buy = ({ }) => {
   return (
     <Layout title="购买">
-
+      <div className='px-20'>
+        <Steps steps={steps} curIndex={2} />
+      </div>
     </Layout>
   );
 }
