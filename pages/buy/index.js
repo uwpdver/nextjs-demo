3d9937563e919d0fcfc8e573991dbba65de2fe8d
zhihao <lzh1119548217@outlook.com>
2022-03-29 12:25:26 +0800

    feat: 增加占位的省市区选择器

--- pages/buy/index.js
+++ pages/buy/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import chevronDownIcon from '@fluentui/svg-icons/icons/chevron_down_20_filled.svg';
+
 import Layout from "../../components/layout";
 import Steps from '../../components/steps';
 
@@ -29,10 +31,11 @@ const Buy = ({ }) => {
       alert(JSON.stringify(values, null, 2));
     },
   })
+
   return (
     <Layout title="购买">
       <div className='px-20 pb-10'>
-        <Steps steps={steps} curIndex={2}  className='my-8'/>
+        <Steps steps={steps} curIndex={2} className='my-8' />
         <div>
           <form onSubmit={formik.handleSubmit} className="flex flex-col w-96 mx-auto" >
             <label htmlFor='name' className='mb-2'>收货人姓名</label>
@@ -56,6 +59,32 @@ const Buy = ({ }) => {
               value={formik.values.phone}
               className="border border-gray-800 p-1 rounded"
             />
+
+            <label htmlFor='province-city-distict' className='mb-2 mt-4'>地区</label>
+            <div className='flex '>
+              <select className='flex-1 border border-gray-800 py-1 pl-2 pr-8 rounded-l appearance-none' name='province'>
+                <option value>省</option>
+                <option value="广东">广东</option>
+              </select>
+              <select className='flex-1 border-y border-gray-800 py-1 pl-2 pr-8 appearance-none' name='city'>
+                <option value>市</option>
+                <option value="广州">广州</option>
+              </select>
+              <select className='flex-1 border border-gray-800 py-1 pl-2 pr-8 rounded-r appearance-none' name="distict">
+                <option value>区</option>
+                <option value="天河">天河</option>
+              </select>
+              <style jsx>
+                {`
+                  select {
+                    background-image: url(${chevronDownIcon.src});
+                    background-size: 20px;
+                    background-repeat: no-repeat;
+                    background-position: center right 8px;                    
+                  }
+                `}
+              </style>
+            </div>
 
             <label htmlFor='address' className='mb-2 mt-4'>详细地址</label>
             <input
