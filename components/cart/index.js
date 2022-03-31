ca87fcd39e1f9627126686b0400596230ff1ad23
zhihao <lzh1119548217@outlook.com>
2022-03-31 12:31:49 +0800

    refacotr(components/cart): 整理代码

--- components/cart/index.js
+++ components/cart/index.js
@@ -5,14 +5,114 @@ import Image from 'next/image';
 import { Formik, Field, Form } from 'formik';
 import { useRouter } from 'next/router';
 import DismissIcon from "@fluentui/svg-icons/icons/dismiss_20_regular.svg";
-import checkmarkCircleIcon from "@fluentui/svg-icons/icons/checkmark_circle_20_regular.svg";
-import checkmarkCircleFilledIcon from "@fluentui/svg-icons/icons/checkmark_circle_20_filled.svg";
 import { CartContext } from "../../pages/_app";
 import Empty from '../empty';
+import CheckIcon from "../check_icon";
 
 export default function Cart({ isOpen, onClose }) {
   const { cart } = useContext(CartContext);
   const router = useRouter();
+
+  const formRender = ({ values, setFieldValue, isSubmitting, isValid }) => {
+    const listItemRender = ({ id, cover, name, size, color, price }) => {
+      const idStr = id.toString();
+      const checked = values.checked.includes(idStr);
+      return (
+        <li key={id} className="py-4">
+          <div className="flex speace-x-2">
+
+            <div className="flex-shrink-0 flex justify-center items-center">
+              <label className="relative cursor-pointer">
+                <Field type="checkbox" name="checked" value={idStr} className="absolute top-0 left-0 opacity-0" />
+                <CheckIcon checked={checked} />
+              </label>
+            </div>
+
+            <div className="flex-shrink-0 relative w-32 h-32">
+              <Link href={`/product/${id}`}>
+                <Image src={cover} layout="fill" objectFit="contain" />
+              </Link>
+            </div>
+
+            <div className="flex-1">
+              <Link href={`/product/${id}`} className="mb-2">
+                {name}
+              </Link>
+              <div className="mb-2 text-gray-500 text-sm">
+                {`${size?.content}/${color?.content?.colorName}`}
+              </div>
+              <div>￥{price}</div>
+            </div>
+          </div>
+        </li >
+      )
+    }
+
+    const listRender = () => (
+      <ul className="divide-y flex-1 overflow-y-auto scrollbar-none">
+        {cart.length ? cart.map(listItemRender) : <Empty className=" mt-24" />}
+      </ul>
+    )
+
+    const footerRender = () => {
+      const onCheckAllChange = () => {
+        setFieldValue('checked', values.checkAll ? [] : cart.map(({ id }) => id.toString()));
+        setFieldValue('checkAll', !values.checkAll);
+      }
+
+      const total = cart
+        .filter(({ id }) => values.checked.includes(id.toString()))
+        .reduce((acc, cur) => acc + cur.price, 0);
+
+      return (
+        <footer>
+          <div className="flex items-center space-x-4">
+            <label className="flex-shrink-0 flex justify-center items-center cursor-pointer">
+              <Field
+                type="checkbox"
+                name="checkAll"
+                value="checkAll"
+                onChange={onCheckAllChange}
+                checked={values.checkAll}
+                className="absolute top-0 left-0 opacity-0"
+              />
+              <CheckIcon checked={values.checkAll} />
+            </label>
+            <div className="text-lg font-bold">{`总计：￥${total}`}</div>
+          </div>
+
+          <button
+            className="block w-full py-2 px-4 mt-4 rounded border-gray-600 text-white bg-gray-800 hover:bg-gray-600 disabled:opacity-30"
+            disabled={!isValid || isSubmitting}
+            type="submit"
+          >
+            去结算
+          </button>
+        </footer>
+      )
+    }
+
+    return (
+      <Form class="flex-1 flex flex-col">
+        {listRender()}
+        {footerRender()}
+      </Form>
+    )
+  }
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.checked.length) {
+      errors.checked = '您还未选择任何商品';
+    }
+    return errors;
+  }
+
+  const onSubmit = async (values) => {
+    alert(JSON.stringify(values, null, 2));
+    router.push('/buy');
+    onClose();
+  }
 
   return (
     <div
@@ -33,91 +133,10 @@ export default function Cart({ isOpen, onClose }) {
           checkAll: false,
           checked: [],
         }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.checked.length) {
-            errors.checked ='您还未选择任何商品';
-          }
-          return errors;
-        }}
-        onSubmit={async (values) => {
-          alert(JSON.stringify(values, null, 2));
-          router.push('/buy');
-          onClose();
-        }}
+        validate={validate}
+        onSubmit={onSubmit}
       >
-        {({ values, setFieldValue, isSubmitting, isValid }) => {
-          const total = cart
-            .filter(({ id }) => values.checked.includes(id.toString()))
-            .reduce((acc, cur) => acc + cur.price, 0);
-
-          return (
-            <Form class="flex-1 flex flex-col">
-              <ul className="divide-y flex-1 overflow-y-auto scrollbar-none">
-                {cart.length === 0
-                  ? <Empty className=" mt-24" />
-                  : cart.map((item) => (
-                    <li key={item.id} className="py-4">
-                      <div className="flex speace-x-2">
-                        <div className="flex-shrink-0 flex justify-center items-center">
-                          <label className="relative">
-                            <Field type="checkbox" name="checked" value={item.id.toString()} className="absolute top-0 left-0 opacity-0" />
-                            <Image src={values.checked.includes(item.id.toString()) ? checkmarkCircleFilledIcon : checkmarkCircleIcon} width={20} height={20} />
-                          </label>
-                        </div>
-
-                        <div className="flex-shrink-0 relative w-32 h-32">
-                          <Link href={`/product/${item.id}`}>
-                            <Image src={item.cover} layout="fill" objectFit="contain" />
-                          </Link>
-                        </div>
-
-                        <div className="flex-1">
-                          <Link href={`/product/${item.id}`} className="mb-2">
-                            {item.name}
-                          </Link>
-                          <div className="mb-2 text-gray-500 text-sm">
-                            {`${item.size?.content}/${item.color?.content?.colorName}`}
-                          </div>
-                          <div>￥{item.price}</div>
-                        </div>
-                      </div>
-                    </li >
-                  ))
-                }
-              </ul>
-
-              <div className="flex items-center space-x-4">
-                <div className="flex-shrink-0 flex justify-center items-center">
-                  <label >
-                    <Field
-                      type="checkbox"
-                      name="checkAll"
-                      value="checkAll"
-                      onChange={
-                        (e) => {
-                          setFieldValue('checked', e.target.value === 'false' ? cart.map(({ id }) => id.toString()) : []);
-                          setFieldValue('checkAll', e.target.value === 'false');
-                        }
-                      }
-                      checked={values.checkAll}
-                      className="absolute top-0 left-0 opacity-0"
-                    />
-                    <Image src={values.checkAll ? checkmarkCircleFilledIcon : checkmarkCircleIcon} width={20} height={20} />
-                  </label>
-                </div>
-                <div className="text-lg font-bold">{`总计：￥${total}`}</div>
-                <button
-                  className="flex-1 py-2 px-4 rounded border-gray-600 text-white bg-gray-800 hover:bg-gray-600 disabled:opacity-30"
-                  disabled={!isValid || isSubmitting}
-                  type="submit"
-                >
-                  去结算
-                </button>
-              </div>
-            </Form>
-          )
-        }}
+        {formRender}
       </Formik>
 
       <style jsx global>{`
