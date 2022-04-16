be0e856363469d5e42811c0a801583c97a145760
zhihao <1119548217@qq.com>
2022-04-16 21:15:32 +0800

    fix: 修复的省市区选择器宽度问题

--- pages/buy/index.tsx
+++ pages/buy/index.tsx
@@ -75,7 +75,7 @@ export default function Buy() {
             <label htmlFor="province-city-distict" className="mb-2 mt-4">
               地区
             </label>
-            <div className="flex ">
+            <div className="flex">
               <style jsx>
                 {`
                   select {
@@ -86,45 +86,51 @@ export default function Buy() {
                   }
                 `}
               </style>
-              <select
-                className="flex-1 border border-gray-800 py-1 pl-2 pr-8 rounded-l appearance-none"
-                name="province"
-                value={formik.values.province}
-                onChange={formik.handleChange}
-              >
-                <option value="">省</option>
-                {provinceOptions.map((key) => (
-                  <option value={key} key={key}>
-                    {key}
-                  </option>
-                ))}
-              </select>
-              <select
-                className="flex-1 border-y border-gray-800 py-1 pl-2 pr-8 appearance-none"
-                name="city"
-                value={formik.values.city}
-                onChange={formik.handleChange}
-              >
-                <option value="">市</option>
-                {cityOptions.map((key) => (
-                  <option value={key} key={key}>
-                    {key}
-                  </option>
-                ))}
-              </select>
-              <select
-                className="flex-1 border border-gray-800 py-1 pl-2 pr-8 rounded-r appearance-none"
-                value={formik.values.distict}
-                onChange={formik.handleChange}
-                name="distict"
-              >
-                <option value="">区</option>
-                {distictOptions.map((key) => (
-                  <option value={key} key={key}>
-                    {key}
-                  </option>
-                ))}
-              </select>
+              <div className="flex-1 h-8 rounded-l border border-gray-800 relative overflow-hidden">
+                <select
+                  className="absolute top-0 left-0 w-full h-full py-1 pl-2 pr-8 appearance-none"
+                  name="province"
+                  value={formik.values.province}
+                  onChange={formik.handleChange}
+                >
+                  <option value="">省</option>
+                  {provinceOptions.map((key) => (
+                    <option value={key} key={key}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="flex-1 h-8 border-y border-gray-800 relative">
+                <select
+                  className="absolute top-0 left-0 w-full h-full py-1 pl-2 pr-8 appearance-none"
+                  name="city"
+                  value={formik.values.city}
+                  onChange={formik.handleChange}
+                >
+                  <option value="">市</option>
+                  {cityOptions.map((key) => (
+                    <option value={key} key={key}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="flex-1 h-8  rounded-r border border-gray-800 relative overflow-hidden">
+                <select
+                  className="absolute top-0 left-0 w-full h-full py-1 pl-2 pr-8 appearance-none"
+                  value={formik.values.distict}
+                  onChange={formik.handleChange}
+                  name="distict"
+                >
+                  <option value="">区</option>
+                  {distictOptions.map((key) => (
+                    <option value={key} key={key}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
 
             <label htmlFor="address" className="mb-2 mt-4">
